cbe5eee72a7e911fc38b18f8a6dbcceb22af910d
LuAnderson <lucasand96@gmail.com>
2017-12-06 22:18:11 -0200

    arquivos base - projeto em angular

---
+++ public/js/controllers/home-controller.js
@@ -0,0 +1,6 @@
+angular.module('simulado').controller('HomeController', function($scope){
+    $scope.foto = {
+        titulo: "Lucas",
+        url: "https://pluralsight.imgix.net/paths/path-icons/angular-14a0f6532f.png"
+    };
+});
