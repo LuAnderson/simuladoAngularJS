2f2b3da61bfdbc69698ed3a89fc162abb5044cb8
LuAnderson <lucasand96@gmail.com>
2017-12-17 05:26:50 -0200

    Aplicando angular na lista de simulados (incompleto)

--- public/js/controllers/lista-controller.js
+++ public/js/controllers/lista-controller.js
@@ -1,5 +1,21 @@
 angular.module('simulado').controller('ListaController', function($scope){
     $scope.lista = {
-        titulo: "Simulado 2018",
+        titulo: "Simulado de 2018",
     }
+
+    $scope.opcoes = [
+        { titulo: "Simulado 10", ativo: false},
+        { titulo: "Simulado 9", ativo: false},
+        { titulo: "Simulado 8", ativo: false},
+        { titulo: "Simulado 7", ativo: false},
+        { titulo: "Simulado 6", ativo: false},
+        { titulo: "Simulado 5", ativo: false},
+        { subtitulo: "Prova objetiva", ativo: true},
+        { subtitulo: "Prova final", ativo: false},
+        { titulo: "Simulado 4", ativo: false},
+        { titulo: "Simulado 3", ativo: false},
+        { titulo: "Simulado 2", ativo: false},
+        { titulo: "Simulado 1", ativo: false},
+        
+    ]
 })
