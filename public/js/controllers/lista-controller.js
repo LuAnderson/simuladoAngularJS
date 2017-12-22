9fcf850a40ddfc53c0dc629ca63c37602335a397
LuAnderson <lucasand96@gmail.com>
2017-12-21 23:11:29 -0200

    ajustes

--- public/js/controllers/lista-controller.js
+++ public/js/controllers/lista-controller.js
@@ -10,12 +10,16 @@ angular.module('simulado').controller('ListaController', function($scope){
         { titulo: "Simulado 7", ativo: false},
         { titulo: "Simulado 6", ativo: false},
         { titulo: "Simulado 5", ativo: false},
-        { subtitulo: "Prova objetiva", ativo: true},
-        { subtitulo: "Prova final", ativo: false},
+        { titulo: "Prova objetiva", ativo: true},
+        { titulo: "Prova final", ativo: false},
         { titulo: "Simulado 4", ativo: false},
         { titulo: "Simulado 3", ativo: false},
         { titulo: "Simulado 2", ativo: false},
         { titulo: "Simulado 1", ativo: false},
-        
+    ]
+
+    $scope.subopcoes = [
+        { subtitulo: "Prova objetiva", ativo: true },
+        { subtitulo: "Prova final", ativo: false },
     ]
 })
